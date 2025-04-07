0f6f99a18602751e41a252d23320d4b94d934715
ZabihollahNamazi <zabihnamazi@gmail.com>
2025-04-07 21:45:13 +0100

    function for adding data to local storage

--- shared-bookmarks/script.js
+++ shared-bookmarks/script.js
@@ -4,13 +4,13 @@
 // Note that when running locally, in order to open a web page which uses modules, you must serve the directory over HTTP e.g. with https://www.npmjs.com/package/http-server
 // You can't open the index.html file using a file:// URL.
 
-import { getUserIds } from "./storage.js";
-document.getElementById("form-input").style.display = "none";
+import { getUserIds, setData, getData } from "./storage.js";
+document.getElementById("form-input").style.display = "none"; // set input form as hide in default
 
 function selectUser(){
   const users = getUserIds();
   let dropdown = document.getElementById("dropdown-users");
-  for(let i =0; i < users.length; i++){
+  for(let i =0; i < users.length; i++){ // adding users to dropdown list
     let option = document.createElement("option");
     option.value = users[i];
     option.id = users[i];
@@ -19,12 +19,30 @@ function selectUser(){
   }
   dropdown.addEventListener("change", (e)=>{
     e.preventDefault();
-    document.getElementById("form-input").style.display = "block";
+    document.getElementById("form-input").style.display = "block"; // when you select a user form will show up
+    const userId = dropdown.options[dropdown.selectedIndex].id; // getting user id from selected option or user
+    
   })
 }
 
+function addData(userId){
+  document.getElementById("form-input").addEventListener("submit", (event)=>{
+    event.preventDefault();
+    const newData = {  // getting data from form input
+      topic: document.getElementById("topic").value, 
+      link: document.getElementById("link").value,
+      desc: document.getElementById("desc").value
+    }
+    const rawData = getData(userId); // get users data from local storage
+    let localData = Array.isArray(rawData) ? rawData : rawData != null ? [rawData] : []; // check if data is an array
+    localData.push(newData) //push the new data into users data which we got from local storage
+    setData(userId, localData); // add users data back to local storage
+  })
+
+}
+
 window.onload = function () {
-  console.log(getUserIds())
+
   selectUser();
-  // document.querySelector("body").innerText = `There are ${users.length} users`;
+
 };
